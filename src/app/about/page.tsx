69bb3f93f6e24e0892a890161a99c843ff07ea55
prabhatbhusal <prabhatbhusal777@gmail.com>
2025-04-18 10:35:55 +0545

    Files Added

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -3,8 +3,18 @@ import React from 'react'
 const page = () => {
   return (
     <div>
-      <div className='h-100 w-full bg-red-800'></div>
-      <div className='h-100 w-full bg-indigo-900'></div>
+      <div className={`h-100 w-full bg-[#23918BA6] flex flex-col justify-center items-center bg-[url(/images/image2.png)] bg-blend-overlay text-white`}>
+        <h1 className='text-xl font-black '>ONE CRUISE AT A TIME</h1>
+        <p  className='text-md font-medium'>Where every journey is as unforgettable as the </p>
+        <p className='text-md font-medium'>destination- your passport to all things cruising.</p>
+      </div>
+      <div className={`h-100 w-full bg-[#23918BA6] flex flex-col justify-center items-center bg-[url(/images/image3.png)] bg-blend-multiply text-white p-40 gap-5`}>
+        <h1 className='text-2xl font-black '>a tale form the sea</h1>
+        <h1  className='text-2xl font-medium'>Set Sail with Unforgettable Experiences </h1>
+        <p className='text-md '>Not just a journey — it’s a collection of once-in-a-lifetime moments. Imagine waking up to a new horizon each day, dining under the stars, exploring exotic ports, and sharing laughter with fellow travelers. Whether you're chasing sunsets in the Caribbean, discovering hidden gems in the Mediterranean, or relaxing on a luxury deck with a drink in hand — unforgettable is just the beginning.</p>
+        <p className='text-md '>Come aboard and create memories that stay with you long after the waves fade.</p>
+      </div>
+      
     </div>
   )
 }
--- src/components/ui/Navbar.tsx
+++ src/components/ui/Navbar.tsx
@@ -3,7 +3,7 @@ import React from "react";
 const Navbar = () => {
   return (
     <nav >
-      <ul className='flex flex-roww justify-around h-15 w-full items-center bg-teal-600 fixed'>
+      <ul className='flex flex-roww justify-around h-15 w-full items-center bg-red-600 fixed'>
         <li>Home</li>
         <li>About</li>
         <li>Services</li>
