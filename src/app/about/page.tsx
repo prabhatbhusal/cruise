6eb595af42e4cc1890df582225db9ca88f6b3afb
prabhatbhusal <prabhatbhusal777@gmail.com>
2025-04-18 09:07:55 +0545

    Files Added

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -3,7 +3,8 @@ import React from 'react'
 const page = () => {
   return (
     <div>
-      
+      <div className='h-100 w-full bg-red-800'></div>
+      <div className='h-100 w-full bg-indigo-900'></div>
     </div>
   )
 }
--- src/components/ui/Navbar.tsx
+++ src/components/ui/Navbar.tsx
@@ -3,7 +3,7 @@ import React from "react";
 const Navbar = () => {
   return (
     <nav >
-      <ul className='flex flex-roww justify-around h-15 items-center bg-teal-600 sticky'>
+      <ul className='flex flex-roww justify-around h-15 w-full items-center bg-teal-600 fixed'>
         <li>Home</li>
         <li>About</li>
         <li>Services</li>
